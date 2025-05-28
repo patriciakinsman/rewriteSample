4fb71c0c11338bf2d77bce35299a06dd5ca51638
PKinsman <PKinsman@PKINSMAN-10L.nbme.org>
2025-05-28 16:29:30 -0400

    Added GradeUtils and student_scores.csv

--- testingCoPilotMaven/src/main/java/testingCoPilotMaven/MyCopilot.java
+++ testingCoPilotMaven/src/main/java/testingCoPilotMaven/MyCopilot.java
@@ -2,8 +2,10 @@ package testingCoPilotMaven;
 
 public class MyCopilot {
 	
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+		GradeUtils.readScores();
 	}
 
 }
